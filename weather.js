1b9671a1d5c641eccc6bea899dc6dbfc40ea414d
ceruleanpurple <theodore.bruckbauer@kellogg.northwestern.edu>
2017-10-24 14:40:54 -0500

    Now uses geolocation

--- weather.js
+++ weather.js
@@ -1,13 +1,18 @@
 
 let getWeather = function(info) {
+  let latitude = info.coords.latitude;
+  let longitude = info.coords.longitude;
+  console.log("latitude = " + latitude);
+  console.log("longitude = " + longitude);
   // console.info(info);
-  let latitude = '41.8781';
-  let longitude = '-87.6298';
+  // let latitude = '41.8781';
+  // let longitude = '-87.6298';
   let openweathermap_api_url = 'https://api.openweathermap.org/data/2.5/weather?'
   openweathermap_api_url += 'lat=' + latitude
   openweathermap_api_url += '&lon=' + longitude
   openweathermap_api_url +='&appid=4ce6f502d38ddae567bf1702b05e168c&units=imperial'
   // console.log(openweathermap_api_url);
+
   fetch(openweathermap_api_url).then(convertToJSON).then(updateWeather).catch(displayError);
 }
 
@@ -26,15 +31,20 @@ let updateWeather = function(junk) {
   // console.log(temp);
   // console.log(desc);
   // console.log(icon);
-  // console.log(junk);
+  console.log(junk);
 
   let div = document.getElementById("weather");
   imgUrl = "http://openweathermap.org/img/w/" + icon + ".png";
   div.querySelector(".card-img-top").src = imgUrl;
   div.querySelector(".card-title").innerHTML = name;
   div.querySelector(".card-text").innerHTML = "It is " + temp + " degrees outside.";
-
   // console.log(imgUrl);
+  lat = junk.coord.lat;
+  lon = junk.coord.lon;
+  console.log(lat);
+  console.log(lon);
+
+
   }
 
 let displayError = function(error) {
@@ -47,6 +57,7 @@ link.addEventListener("click", function(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(getWeather);
 });
+
 
 // HINT:
 // Weather icon example: http://openweathermap.org/img/w/10d.png
