017e9c7c7f611941e17ed4e578e58ce2cad510b8
ceruleanpurple <theodore.bruckbauer@kellogg.northwestern.edu>
2017-10-24 14:04:46 -0500

    Clean up

--- weather.js
+++ weather.js
@@ -1,14 +1,13 @@
 
 let getWeather = function(info) {
-  console.info(info);
-  // let div = document.getElementById("weather");
+  // console.info(info);
   let latitude = '41.8781';
   let longitude = '-87.6298';
   let openweathermap_api_url = 'https://api.openweathermap.org/data/2.5/weather?'
   openweathermap_api_url += 'lat=' + latitude
   openweathermap_api_url += '&lon=' + longitude
   openweathermap_api_url +='&appid=4ce6f502d38ddae567bf1702b05e168c&units=imperial'
-  console.log(openweathermap_api_url);
+  // console.log(openweathermap_api_url);
   fetch(openweathermap_api_url).then(convertToJSON).then(updateWeather).catch(displayError);
 }
 
@@ -39,7 +38,7 @@ let updateWeather = function(junk) {
   }
 
 let displayError = function(error) {
-  console.debug(error);
+  // console.debug(error);
   window.alert("Sorry, something went wrong.");
 }
 
